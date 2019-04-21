f4ccf8eac11e2c2ce935003477250a9389268027
SgrAlpha <SgrAlpha@users.noreply.github.com>
2019-04-21 11:16:37 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env sh
   
 docker pull alpine:latest
-docker build -t sgrio/java:jdk_11_alpine alpine/jdk/
-docker tag sgrio/java:jdk_11_alpine sgrio/java:latest_alpine
+docker build -t sgrio/java:jdk_12_alpine alpine/jdk/
+docker tag sgrio/java:jdk_12_alpine sgrio/java:latest_alpine
 
 docker pull centos:latest
-docker build -t sgrio/java:jdk_11_centos centos/jdk/
-docker tag sgrio/java:jdk_11_centos sgrio/java:latest_centos
+docker build -t sgrio/java:jdk_12_centos centos/jdk/
+docker tag sgrio/java:jdk_12_centos sgrio/java:latest_centos
 
 docker pull centos:latest
-docker build -t sgrio/java:jdk_11_ubuntu ubuntu/jdk/
-docker tag sgrio/java:jdk_11_ubuntu sgrio/java:latest_ubuntu
+docker build -t sgrio/java:jdk_12_ubuntu ubuntu/jdk/
+docker tag sgrio/java:jdk_12_ubuntu sgrio/java:latest_ubuntu
 
-docker tag sgrio/java:jdk_11_alpine sgrio/java:jdk_11
-docker tag sgrio/java:jdk_11 sgrio/java:latest
+docker tag sgrio/java:jdk_12_alpine sgrio/java:jdk_12
+docker tag sgrio/java:jdk_12 sgrio/java:latest
 
-docker push sgrio/java:jdk_11_alpine
+docker push sgrio/java:jdk_12_alpine
 docker push sgrio/java:latest_alpine
 
-docker push sgrio/java:jdk_11_centos
+docker push sgrio/java:jdk_12_centos
 docker push sgrio/java:latest_centos
 
-docker push sgrio/java:jdk_11_ubuntu
+docker push sgrio/java:jdk_12_ubuntu
 docker push sgrio/java:latest_ubuntu
 
-docker push sgrio/java:jdk_11
+docker push sgrio/java:jdk_12
 docker push sgrio/java:latest
 
