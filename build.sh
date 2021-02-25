bacbae4633fe6c1b3a218fb61bdea130e6b140c2
SgrAlpha <admin@sgr.io>
2021-02-25 16:32:33 +0800

    Support 15.0.2

--- build.sh
+++ build.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env sh
 
 docker pull sgrio/java:latest
-docker pull sgrio/java:jdk_14
+docker pull sgrio/java:jdk_15
 docker pull sgrio/java:latest_alpine
-docker pull sgrio/java:jdk_14_alpine
+docker pull sgrio/java:jdk_15_alpine
 docker pull sgrio/java:latest_centos
-docker pull sgrio/java:jdk_14_centos
+docker pull sgrio/java:jdk_15_centos
 docker pull sgrio/java:latest_ubuntu
-docker pull sgrio/java:jdk_14_ubuntu
+docker pull sgrio/java:jdk_15_ubuntu
 
 docker pull alpine:latest
-docker build -t sgrio/java:jdk_14_alpine alpine/jdk/
-docker tag sgrio/java:jdk_14_alpine sgrio/java:latest_alpine
+docker build -t sgrio/java:jdk_15_alpine alpine/jdk/
+docker tag sgrio/java:jdk_15_alpine sgrio/java:latest_alpine
 
 docker pull centos:latest
-docker build -t sgrio/java:jdk_14_centos centos/jdk/
-docker tag sgrio/java:jdk_14_centos sgrio/java:latest_centos
+docker build -t sgrio/java:jdk_15_centos centos/jdk/
+docker tag sgrio/java:jdk_15_centos sgrio/java:latest_centos
 
 docker pull ubuntu:latest
-docker build -t sgrio/java:jdk_14_ubuntu ubuntu/jdk/
-docker tag sgrio/java:jdk_14_ubuntu sgrio/java:latest_ubuntu
+docker build -t sgrio/java:jdk_15_ubuntu ubuntu/jdk/
+docker tag sgrio/java:jdk_15_ubuntu sgrio/java:latest_ubuntu
 
-docker tag sgrio/java:jdk_14_alpine sgrio/java:jdk_14
-docker tag sgrio/java:jdk_14 sgrio/java:latest
+docker tag sgrio/java:jdk_15_alpine sgrio/java:jdk_14
+docker tag sgrio/java:jdk_15 sgrio/java:latest
 
-docker push sgrio/java:jdk_14_alpine
+docker push sgrio/java:jdk_15_alpine
 docker push sgrio/java:latest_alpine
 
-docker push sgrio/java:jdk_14_centos
+docker push sgrio/java:jdk_15_centos
 docker push sgrio/java:latest_centos
 
-docker push sgrio/java:jdk_14_ubuntu
+docker push sgrio/java:jdk_15_ubuntu
 docker push sgrio/java:latest_ubuntu
 
-docker push sgrio/java:jdk_14
+docker push sgrio/java:jdk_15
 docker push sgrio/java:latest
 
